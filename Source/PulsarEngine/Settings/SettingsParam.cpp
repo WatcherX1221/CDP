7b76f03ab7621aea1e973099f33496473ebfac80
Blockface3 <lukemundy2005@btinternet.com>
2026-05-18 13:55:40 +0100

    Updated settings menus

--- Source/PulsarEngine/Settings/SettingsParam.cpp
+++ Source/PulsarEngine/Settings/SettingsParam.cpp
@@ -8,32 +8,37 @@ namespace Pulsar {
 namespace Settings {
 
 u8 Params::radioCount[Params::pageCount] ={
-    3, 5, 4, 2, 4, 2, 1 //menu, race, host, KO, OTT, game, tc
-    //Add user radio count here
-
+    2, 4, 5, 4, // PUL  // Misc, Race, Host, OTTKO
+    4, 1, 2, 1  // USER // Audio, Item, Physics, Lap
 };
-u8 Params::scrollerCount[Params::pageCount] ={ 1, 1, 1, 2, 2, 3, 2 }; //menu, race, host, KO, OTT, game, tc
+
+u8 Params::scrollerCount[Params::pageCount] ={
+    1, 1, 0, 2, // PUL  // Misc, Race, Host, OTTKO
+    0, 3, 2, 2  // USER // Audio, Item, Physics, Lap
+};
 
 u8 Params::buttonsPerPagePerRow[Params::pageCount][Params::maxRadioCount] = //first row is PulsarSettingsType, 2nd is rowIdx of radio
 {
-    { 2, 2, 3, 0, 0, 0 }, //Menu
-    { 2, 2, 2, 2, 3, 0 }, //Race
-    { 3, 4, 2, 2, 0, 0 }, //Host
-    { 2, 2, 0, 0, 0, 0 }, //KO
-    { 2, 2, 2, 2, 0, 0 }, //OTT
-    { 3, 3, 0, 0, 0, 0 },  //Game
-    { 2, 0, 0, 0, 0, 0 }, //TC
+    { 2, 2, 0, 0, 0, 0 }, // Misc
+    { 2, 2, 2, 3, 0, 0 }, // Race
+    { 3, 2, 2, 2, 2, 0 }, // Host
+    { 2, 2, 2, 2, 0, 0 }, // OTTKO
+    { 3, 2, 2, 2, 0, 0 }, // Audio
+    { 2, 0, 0, 0, 0, 0 }, // Item
+    { 3, 3, 0, 0, 0, 0 }, // Physics
+    { 3, 0, 0, 0, 0, 0 }  // Lap
 };
 
 u8 Params::optionsPerPagePerScroller[Params::pageCount][Params::maxScrollerCount] =
 {
-    { 5, 7, 0, 0, 0}, //Menu
-    { 4, 0, 0, 0, 0}, //Race
-    { 11, 0, 0, 0, 0}, //Host
-    { 4, 4, 0, 0, 0}, //KO
-    { 16, 7, 0, 0, 0}, //OTT
-    { 6, 12, 9, 0, 0},  //Game
-    { 4, 4, 0, 0, 0}, //WDDTC
+    {  5,  7,  0,  0,  0}, // Misc
+    {  4,  0,  0,  0,  0}, // Race
+    {  0,  0,  0,  0,  0}, // Host
+    {  4,  4,  0,  0,  0}, // OTTKO
+    {  0,  0,  0,  0,  0}, // Audio
+    { 10,  5, 23,  0,  0}, // Item
+    { 10, 12,  0,  0,  0}, // Physics
+    { 11,  9,  0,  0,  0}  // Lap
 };
 
 }//namespace Settings
